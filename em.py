2689ecd04a4ab352e39905c4fe505311ea118962
xingray <xingrui@gmail.com>
2017-07-24 21:29:27 +0800

    add new file

---
+++ em.py
@@ -0,0 +1,25 @@
+#to implement em example of the following link: https://www.zhihu.com/question/27976634/answer/39132183
+
+import numpy as np
+
+
+INPUT_DATA = [[0, 1, 1, 1, 0, 0, 1, 0, 1, 0],
+              [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+              [0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+              [0, 1, 0, 1, 1, 1, 0, 0, 1, 1],
+              [1, 0, 0, 0, 1, 0, 0. 0, 1, 0]]
+
+
+if __name__ == "__main__":
+    print('Begin Training')
+
+    theta_a = 0.60
+    theta_b = 0.50
+
+    for i in xrange(1, 100):
+        #E step
+
+    print('Stop Training')
+
+
+
